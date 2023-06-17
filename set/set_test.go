eef4df1a924453a9c09ee570a843baecd1900544
siyuxiong(熊思宇) <siyuxiong@futunn.com>
2023-06-17 21:15:18 +0800

    add unittest

--- set/set_test.go
+++ set/set_test.go
@@ -29,3 +29,23 @@ func TestNewSet(t *testing.T) {
 		})
 	}
 }
+
+func TestNewConcurrencySet(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "TEST",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := NewConcurrencySet[string]()
+			s.Insert("test")
+			assert.Equal(t, true, s.Exist("test"))
+			assert.Equal(t, false, s.Exist("test no"))
+			s.Delete("test")
+			assert.Equal(t, false, s.Exist("test"))
+		})
+	}
+}
